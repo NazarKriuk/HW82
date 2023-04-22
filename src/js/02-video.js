b0e30b2cb8feee2fac45c54520890ca9cb21d87f
NazarKriuk <nazar_19992911@ukr.net>
2023-04-22 21:11:31 +0300

    321

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframeEl = document.querySelector('iframe');
+const player = new Player(iframeEl);
+
+const savedTime = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+if (savedTime) {
+  player.setCurrentTime(savedTime.seconds);
+}
+
+player.on('timeupdate', throttle(onPlayerStart, 1000));
+
+function onPlayerStart(e) {
+  localStorage.setItem(
+    STORAGE_KEY,
+    JSON.stringify({
+      seconds: e.seconds,
+    })
+  );
+}
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY))
+  ? JSON.parse(localStorage.getItem(STORAGE_KEY))
+  : {};
+
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('[name="email"]');
+const textarea = document.querySelector('[name="message"]');
+
+savedInputs();
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  if (input.value == '' && textarea.value == '') {
+    return Notify.failure('Please fill out all fields!');
+  }
+  if (input.value.trim() == '' || textarea.value.trim() == '') {
+    return Notify.warning('Please fill out ALL fields!!!');
+  }
+  Notify.success('Thank you! All is well :)');
+  console.log(formData);
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+});
+
+form.addEventListener('input', throttle(onInputChange, 500));
+
+function onInputChange(e) {
+  formData[e.target.name] = e.target.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function savedInputs() {
+  if (formData.email) {
+    input.value = formData.email;
+  }
+  if (formData.message) {
+    textarea.value = formData.message;
+  }
+}
